7d36184fba97ea0410ce80dfd8207ad8725b5829
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-24 20:50:12 -0500

    add user, login-response & auth-status interface

---
+++ src/app/auth/interfaces/auth-status.enum.ts
@@ -0,0 +1,7 @@
+export enum AuthStatus {
+
+    cheking = 'cheking',
+    authenticated = 'authenticated',
+    notAuthenticated = 'notAuthenticated',
+
+}
---
+++ src/app/auth/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+    email:    string;
+    name:     string;
+    isActive: boolean;
+    roles:    string[];
+    _id:      string;
+    __v:      number;
+}
