ed59f91e03f63ca923668ba5c9d093194221ed8a
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 12:24:54 -0500

    add endpoint login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +16,29 @@ export class AuthService {
   private _currentUser = signal<User | null>( null );
   private _authStatus = signal<AuthStatus>( AuthStatus.cheking );
 
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus =computed( () => this._authStatus() );
+
   constructor() { }
   
   login( email: string, password: string ): Observable<boolean> {
 
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
 
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        tap( ({ user, token }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token);
 
-    return of(true);
+          console.log({ user, token });
+        }),
+        map( () => true )
+
+        // TODO: Manejo Errores
+      )
   }
 
 }
