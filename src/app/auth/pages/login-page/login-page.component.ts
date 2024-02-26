97d6264518e908f97b4e966879f9ad6183e8cbe4
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 13:44:01 -0500

    navigate in dashboard component when login on

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,8 +12,9 @@ import Swal from 'sweetalert2'
 })
 export class LoginPageComponent {
 
-  private fb = inject( FormBuilder );
+  private fb          = inject( FormBuilder );
   private authService = inject( AuthService );
+  private router      = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
     email:    ['yefferson.barrial@gmail.com', [ Validators.required, Validators.email ]],
@@ -25,9 +27,7 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
     this.authService.login( email, password )
       .subscribe({
-        next: ( ) => {
-          console.log('!Todo bien!');
-        },
+        next: ( ) => this.router.navigateByUrl('/dashboard') ,
         error: ( errorMessage ) => {
           Swal.fire( 'Error', errorMessage, 'error' )
         }
