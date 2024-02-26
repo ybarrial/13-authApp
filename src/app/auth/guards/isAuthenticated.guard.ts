46caeeb1015a0c335211062b22af63cc4cfa3b21
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 15:02:31 -0500

    implement functional logic of Guard

--- src/app/auth/guards/isAuthenticated.guard.ts
+++ src/app/auth/guards/isAuthenticated.guard.ts
@@ -1,9 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  console.log('isAuthenticatedGuard');
+  
   console.log({ route, state });
 
+  const authService = inject( AuthService );
+  const router      = inject( Router );
 
-  return true;
+  if( authService.authStatus() === AuthStatus.authenticated ) {
+    return true;
+  }
+
+  //const url = state.url;
+  //localStorage.setItem('url', url );
+  router.navigateByUrl('/auth/login')
+
+  return false;
 };
