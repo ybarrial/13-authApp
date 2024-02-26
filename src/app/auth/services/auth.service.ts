75fcfba8106d443c7bc28c9e1dd08805dc45352e
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 15:17:17 -0500

    dont repeat yourself, refactor repeat code or duplicated in a new method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -20,30 +20,33 @@ export class AuthService {
   public authStatus =computed( () => this._authStatus() );
 
   constructor() { }
-  
-  login( email: string, password: string ): Observable<boolean> {
 
+
+  private setAuthentication(user: User, token: string): boolean {
+    
+    this._currentUser.set( user );
+    this._authStatus.set( AuthStatus.authenticated );
+    localStorage.setItem('token', token);
+
+    return true;
+  }
+
+  login( email: string, password: string ): Observable<boolean> {
     const url = `${ this.baseUrl }/auth/login`;
     const body = { email, password };
 
     return this.http.post<LoginResponse>( url, body )
       .pipe(
         tap( ({ user, token }) => {
-          this._currentUser.set( user );
-          this._authStatus.set( AuthStatus.authenticated );
-          localStorage.setItem('token', token);
+          this.setAuthentication(user, token);
         }),
         map( () => true ),
-
         // TODO: Manejo Errores
-        catchError( err => throwError( () => err.error.message )
-        
-        )
+        catchError( err => throwError( () => err.error.message ))
       );
   }
 
   checkAuthStatus(): Observable<boolean> {
-
     const url = `${ this.baseUrl }/auth/check-token`;
     const token = localStorage.getItem('token');
     
@@ -54,12 +57,7 @@ export class AuthService {
 
     return this.http.get<CheckTokenResponse>(url , { headers})
       .pipe(
-        map( ({ token, user }) =>{
-          this._currentUser.set( user );
-          this._authStatus.set( AuthStatus.authenticated );
-          localStorage.setItem('token', token);
-          return true;
-        }),
+        map( ({ token, user }) => this.setAuthentication(user, token)),
         // Error
         catchError( () => {
           this._authStatus.set(AuthStatus.notAuthenticated);
