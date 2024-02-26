b464116bd56ea6596d09a183ef1b2225bcf58f1e
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 13:06:57 -0500

    add catchError inside .pipe with message of backend

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { Observable, map, of, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
 import { AuthStatus, LoginResponse, User } from '../interfaces';
 
@@ -35,10 +35,13 @@ export class AuthService {
 
           console.log({ user, token });
         }),
-        map( () => true )
+        map( () => true ),
 
         // TODO: Manejo Errores
-      )
+        catchError( err => throwError( () => err.error.message )
+        
+        )
+      );
   }
 
 }
