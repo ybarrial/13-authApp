e5c3ccf67cffab402cf7d404a8c188e5c9b5fcf3
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 21:50:11 -0500

    add service logout & implement

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -50,7 +50,10 @@ export class AuthService {
     const url = `${ this.baseUrl }/auth/check-token`;
     const token = localStorage.getItem('token');
     
-    if ( !token ) return of(false);
+    if ( !token ) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${token}`);
@@ -64,6 +67,12 @@ export class AuthService {
           return of(false);
         })
       )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set(null);
+    this._authStatus.set( AuthStatus.notAuthenticated );
   }
 
 }
