b7ef31e4c1b99835f7dc1ad3a6f7558638ec29f5
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 21:32:55 -0500

    use in CanActivate the isNotAuthenticatedGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { isAuthenticatedGuard } from './auth/guards/isAuthenticated.guard';
+import { isNotAuthenticatedGuard, isAuthenticatedGuard } from './auth/guards';
 
 const routes: Routes = [
   {
     path: 'auth',
     //guards
+    canActivate: [ isNotAuthenticatedGuard ],
     loadChildren: () =>  import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
