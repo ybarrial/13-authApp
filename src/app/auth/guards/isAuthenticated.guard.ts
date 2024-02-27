97770b61727c22a297b933734d6a625ed38c86e9
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-26 21:32:23 -0500

    commend unuseful validation

--- src/app/auth/guards/isAuthenticated.guard.ts
+++ src/app/auth/guards/isAuthenticated.guard.ts
@@ -12,9 +12,9 @@ export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
     return true;
   }
 
-  if( authService.authStatus() === AuthStatus.checking ) {
-    return false;
-  }
+  // if( authService.authStatus() === AuthStatus.checking ) {
+  //   return false;
+  // }
 
   //const url = state.url;
   //localStorage.setItem('url', url );
